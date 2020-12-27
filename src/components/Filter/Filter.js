5b129e0708c76b647eab607a1217cc3b8a43fbf0
Kostiantyn Harbuz <konst.garbuz@gmail.com>
2020-12-27 19:52:30 +0200

    добавляет выполнение первого задания

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+
+function Filter({ value, onChange }) {
+    return (
+          <label  >
+            <p>Find contacts by name:</p>
+            <input type="text" value={value} onChange={onChange} />
+          </label>
+    )
+    
+}
+
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+}
+
+export default Filter;
